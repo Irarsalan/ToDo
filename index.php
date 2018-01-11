fb0fc892d3ab575c1d3859582292d80224e25021
Irarsalan <35283470+Irarsalan@users.noreply.github.com>
2018-01-10 22:12:09 -0500

    Adding index.php
    Main page with title and field to enter new task with submit button
    Next step is to make the connection to the database

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title> TODO Application</title> 
+</head>
+
+<body>
+	<h1 style= "text-align: center;"> TODO Application </h1> 
+
+		<div style= "text-align: center;> 
+	<form method= "POST" action="index.php"> 
+		<input type="text" name="task">
+		<input type="submit" value="Add New Task">
+	</form>
+		</div>
+</body>
+</html>
