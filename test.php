a6928480b1092179f2f7228b97edd37ee6362dbf
Irarsalan <insia.ahsan@gmail.com>
2018-01-13 11:58:23 -0500

    Adding function to delete task from database

--- test.php
+++ test.php
@@ -1,13 +1,22 @@
 <?php 
 	$db = mysqli_connect("localhost:3307", "root", "1234", "todoapp");//make a connection to database
 	
-	if (isset($_POST['submit'])) //check to make sure 
+	if (isset($_POST['submit']))//make sure submit button selected
 	{
-		
 			$task = $_POST['task'];
-			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
+			$ddate = $_POST['ddate'];
+			$sql = "INSERT INTO tasks (task,ddate) VALUES ('$task', '$ddate')";
 			mysqli_query($db, $sql);
 			header('location: index.php');
 	
 	}	
+	
+	if (isset($_GET['deletetask'])) 
+	{
+		$taskno = $_GET['deletetask'];
+		$sql = "DELETE FROM tasks WHERE taskid = '$taskno'"; 
+		mysqli_query($db, $sql);
+		header ('location: index.php');
+	}
+	
 	?>
